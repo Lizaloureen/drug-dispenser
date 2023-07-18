7346491f4719130b6dc6f6a45a9469d023f5f216
Ayushi-Savla <ayushi.savla@strathmore.edu>
2023-07-18 18:32:56 +0300

    ak.bjc\

---
+++ view/homepage.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Homepage</title>
+    <link rel="stylesheet" type="text/css" href="C:/xampp1/htdocs/drug dispensing/drug-dispenser/Static/homepage.css">
+</head>
+<body class="body">
+    <div class="navbar">
+        <div class="logo">Logo</div>
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li class="services">
+                <a href="#">Services</a>
+                <ul class="dropdown">
+                    <li>Login</li>
+                    <li>SignUp</li>
+                </ul>
+            </li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+
+    <div class="carousel-container">
+        <div class="carousel">
+            <div class="carousel__face">
+                <img src="C:/xampp1/htdocs/drug dispensing/drug-dispenser/image1.jpeg" alt="Image 1">
+            </div>
+            <div class="carousel__face">
+                <img src="C:/xampp1/htdocs/drug dispensing/drug-dispenser/image2.jpeg" alt="Image 2">
+            </div>
+            <div class="carousel__face">
+                <img src="C:/xampp1/htdocs/drug dispensing/drug-dispenser/image3.jpeg" alt="Image 3">
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col photo-left">
+                <img src="C:/xampp1/htdocs/drug dispensing/drug-dispenser/logo.jpg" alt="Left Photo">
+            </div>
+            <div class="col text-right">
+                <h3>About Us</h3>
+                <p>We are a drug dispensing site which connects doctors, patients, pharmacies and companies to ease the transfer of medicines with reduced physical contact.</p>
+            </div>
+        </div>
+        <div class="text-wrap">
+            <h2>Welcome to My Medicine!</h2>
+            <p>We are dedicated to providing high-quality services to our valued users.</p>
+        </div>
+    </div>
+</body>
+</html>
