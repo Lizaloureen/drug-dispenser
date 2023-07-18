c4ac597e8b203b1d1120c1f587ed7c4628b0423b
Ayushi-Savla <ayushi.savla@strathmore.edu>
2023-07-18 18:58:52 +0300

    Update signup.php

--- config/signup.php
+++ config/signup.php
@@ -2,44 +2,66 @@
 require_once("connection.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $staffno = $_POST["staffno"];
-    $name = $_POST["name"];
-    $prescriptionno = $_POST["prescriptionno"];
-    $salary = $_POST["salary"]; 
-    $bonus = $_POST["bonus"];
     
-    try {
-        $sql = "INSERT INTO staff (staffno, name, prescriptionno, salary, bonus)
-                VALUES (?, ?, ?, ?, ?)";
-        
-        $stmt = $connection->prepare($sql);
-        $stmt->execute([$staffno, $name, $prescriptionno, $salary, $bonus]);
-        
-        echo "Data inserted successfully.";
-    } catch(PDOException $e) {
-        echo "Error: " . $e->getMessage();
+    //patient signup
+    public function patientSignup($patientID, $patientName, $patientPhoneNumber, $Ppassword, $patientAddress, $patientGender){
+        try {
+            $stmt = $this->connection->prepare("INSERT INTO patient (patientID, Name, phoneNumber, password, address, Gender) VALUES (?, ?, ?, ?, ?, ?)");
+            $stmt->execute([$patientID, $patientName, $patientPhoneNumber, $Ppassword, $patientAddress, $patientGender]);
+            return true; // Return true if the signup was successful
+        } catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return false;
+        }        
+    }
+    
+    // Doctor signup
+    public function doctorSignup($docID, $doctorName, $doctorPhoneNumber, $Dpassword, $doctorAddress, $doctorGender){
+        try {
+            $stmt = $this->connection->prepare("INSERT INTO doctor (docID, Name, phoneNumber, password, address, Gender) VALUES (?, ?, ?, ?, ?, ?)");
+            $stmt->execute([$docID, $doctorName, $doctorPhoneNumber, $Dpassword, $doctorAddress, $doctorGender]);
+            return true; // Return true if the signup was successful
+        } catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return false;
+        }        
+    }
+
+    // Pharmaceutical Company signup
+    public function PharmaceuticalCompanySignup($pcID, $pcName, $address, $PhoneNo, $pharmaceuticalPassword){
+        try {
+            $stmt = $this->connection->prepare("INSERT INTO pharmaceuticalcompany(pcID, pcName, address, PhoneNo, Password) VALUES (?, ?, ?, ?, ?)");
+            $stmt->execute([$pcID, $pcName, $address, $PhoneNo, $pharmaceuticalPassword]);
+            return true; // Return true if the signup was successful
+        } catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return false;
+        }        
+    }
+
+    // Pharmacy signup
+    public function PharmacySignup($pharmacyName, $phID, $phoneNo, $profitPercentage, $drugTradeName, $address, $pharmacyPassword){
+        try {
+            $stmt = $this->connection->prepare("INSERT INTO pharmacy (Name, phID, phoneNo, profitPercentage, drugTradeName, address, Password) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $stmt->execute([$pharmacyName, $phID, $phoneNo, $profitPercentage, $drugTradeName, $address, $pharmacyPassword]);
+            return true; // Return true if the signup was successful
+        } catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return false;
+        }        
+    }
+
+    // Staff signup
+    public function StaffSignup($staffno, $name, $prescriptionno, $salary, $bonus, $staffPassword){
+        try {
+            $stmt = $this->connection->prepare("INSERT INTO staff (staffno, name, prescriptionno, salary, bonus, Password) VALUES (?, ?, ?, ?, ?, ?)");
+            $stmt->execute([$staffno, $name, $prescriptionno, $salary, $bonus, $staffPassword]);
+            return true; // Return true if the signup was successful
+        } catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return false;
+        }        
     }
 }
 ?>
 
-<html>
-<head>
-    <title>Register Drug</title>
-</head>
-<body>
-    <h1>Drug Registration</h1>
-    <form action="#" method="POST">
-        <label for="staffno">Staff Number</label><br>
-        <input type="text" name="staffno" required><br>                        
-        <label for="name">Name</label><br>
-        <input type="text" name="name" required><br>
-        <label for="prescriptionno">Prescription Number</label><br>
-        <input type="number" name="prescriptionno" required><br>
-        <label for="salary">Salary</label><br>
-        <input type="number" name="salary" required><br>
-        <label for="bonus">Bonus</label><br>
-        <input type="number" name="bonus" required><br>                        
-        <input type="submit" name="addDrug" value="Submit">
-    </form>
-</body>
-</html>
