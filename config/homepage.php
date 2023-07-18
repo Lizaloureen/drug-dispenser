0cd7cf2c4aafd9535eeaad1a8701da0f95661b91
Ayushi-Savla <ayushi.savla@strathmore.edu>
2023-07-18 18:15:40 +0300

    homepage

---
+++ config/homepage.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Homepage</title>
+    <link rel="stylesheet" type="text/css" href="C:\xampp1\htdocs\drug dispensing\drug-dispenser\Static\homepage.css">
+</head>
+<body class="body">
+    <div class="navbar">
+        <div class="logo">Logo</div>
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li class="services">
+                <a href="#">Services</a>
+                <ul class="dropdown">
+                    <li>Login</li>
+                    <li>SignUp</li>
+                </ul>
+            </li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+
+    <div class="carousel-container">
+    <div class="carousel">
+        <div class="carousel__face">
+            <img src="C:\xampp1\htdocs\drug dispensing\drug-dispenser\image1.jpeg" alt="Image 1">
+        </div>
+        <div class="carousel__face">
+            <img src="C:\xampp1\htdocs\drug dispensing\drug-dispenser\image2.jpeg" alt="Image 2">
+        </div>
+        <div class="carousel__face">
+            <img src="C:\xampp1\htdocs\drug dispensing\drug-dispenser\image3.jpeg" alt="Image 3">
+        </div>
+    </div>
+</div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col photo-left">
+                <!-- Left photo content -->
+            </div>
+            <div class="col text-right">
+                <!-- Right text content -->
+            </div>
+        </div>
+        <div class="text-wrap">
+            <h2>Heading</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque elit ligula, et ultricies ipsum finibus nec.</p>
+        </div>
+        <!-- More content here -->
+    </div>
+
+</body>
+</html>
